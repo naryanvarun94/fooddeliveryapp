66085e924b0ac6f9937d1ef56d083c988fec5e61
varun.nj <varun.nj@i-exceed.com>
2023-08-03 16:43:25 +0530

    minor css and layout changes

--- src/Component/Layout/Header.js
+++ src/Component/Layout/Header.js
@@ -1,18 +1,19 @@
-import {Fragment} from 'react'
-import mealimage from '../../asset/rachel-park-hrlvr2ZlUNk-unsplash.jpg'
-import classes from './Header.module.css'
-import HeaderCartButton from './HeaderCardButton'
-function Header(props){
-    return (
-        <Fragment>
-            <header className={classes.header}>
-                <h1>Foodie</h1>
-                <HeaderCartButton clickHandler={props.clickHandler}/>
-            </header>
-            <div className={classes['main-image']}>
-                <img src={mealimage}/>
-            </div>
-        </Fragment>
-    )
+import { Fragment } from "react";
+import mealimage from "../../asset/rachel-park-hrlvr2ZlUNk-unsplash.jpg";
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCardButton";
+import Icon from "../../asset/foodicon.png";
+function Header(props) {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>Foodie</h1>
+        <HeaderCartButton clickHandler={props.clickHandler} />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealimage} />
+      </div>
+    </Fragment>
+  );
 }
-export default Header
+export default Header;
